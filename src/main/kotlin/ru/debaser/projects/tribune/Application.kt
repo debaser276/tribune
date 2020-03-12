19d54f8f0577cf8e06b7a1e5e084a9245b7fb571
debaser <debaser86@yandex.ru>
2020-03-12 21:02:28 +0300

    Add dependencies, install features

--- src/main/kotlin/ru/debaser/projects/tribune/Application.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/Application.kt
@@ -1,12 +1,36 @@
 package ru.debaser.projects.tribune
 
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.routing.Routing
 import io.ktor.server.cio.EngineMain
+import org.kodein.di.ktor.KodeinFeature
 
 fun main(args : Array<String>) {
     EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+            serializeNulls()
+        }
+    }
+    install(StatusPages) {
 
+    }
+    install(KodeinFeature) {
+
+    }
+    install(Authentication) {
+
+    }
+    install(Routing) {
+
+    }
 }
