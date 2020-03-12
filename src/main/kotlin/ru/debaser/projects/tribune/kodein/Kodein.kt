fcd6feff641a62f6442874bea1da5e50cfa2c724
debaser <debaser86@yandex.ru>
2020-03-12 21:15:49 +0300

    Bind RoutingV1

--- src/main/kotlin/ru/debaser/projects/tribune/kodein/Kodein.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/kodein/Kodein.kt
@@ -2,7 +2,10 @@ package ru.debaser.projects.tribune.kodein
 
 import io.ktor.application.Application
 import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.eagerSingleton
+import ru.debaser.projects.tribune.route.RoutingV1
 
-fun Kodein.MainBuilder.bind(app: Application) {
-
+fun Kodein.MainBuilder.binds(app: Application) {
+    bind<RoutingV1>() with eagerSingleton { RoutingV1() }
 }
