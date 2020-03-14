469c31d9de60f615f5667436337f0a74c7a934d2
debaser <debaser86@yandex.ru>
2020-03-14 09:23:54 +0300

    Add jwtsecret path salt password to application.conf and jwtservice

--- src/main/kotlin/ru/debaser/projects/tribune/service/JWTTokenService.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/service/JWTTokenService.kt
@@ -3,9 +3,24 @@ package ru.debaser.projects.tribune.service
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import org.apache.commons.codec.binary.Hex
+import org.springframework.security.crypto.encrypt.Encryptors
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
 
-class JWTTokenService (private val secret: String) {
+class JWTTokenService (
+    private val password: String,
+    private val salt: String,
+    private val path: String
+) {
+    private lateinit var secret: String
+
+    init {
+        val decryptor = Encryptors.stronger(password, Hex.encodeHexString(salt.toByteArray(Charsets.UTF_8)))
+        secret = String(decryptor.decrypt(Files.readAllBytes(Paths.get(path))))
+    }
+
     private val algo = Algorithm.HMAC256(secret)
 
     val verifier: JWTVerifier = JWT.require(algo).build()
