9158015c94724aad83bb03ee2608e22a3e7cf823
debaser <debaser86@yandex.ru>
2020-03-14 09:59:59 +0300

    Add getByUsername to UserRepositorye

--- src/main/kotlin/ru/debaser/projects/tribune/repository/UserRepository.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/repository/UserRepository.kt
@@ -2,9 +2,11 @@ package ru.debaser.projects.tribune.repository
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import ru.debaser.projects.tribune.dto.AuthenticationResponseDto
 import ru.debaser.projects.tribune.model.UserModel
 
 interface UserRepository {
+    suspend fun getByUsername(username: String): UserModel?
     suspend fun add(user: UserModel): UserModel
     suspend fun save(user: UserModel): UserModel
 }
@@ -13,6 +15,9 @@ class UserRepositoryInMemoryWithMutex: UserRepository {
     private var nextId = 1L
     private val users = mutableListOf<UserModel>()
     private val mutex = Mutex()
+
+    override suspend fun getByUsername(username: String): UserModel? =
+        users.find { it.username == username }
 
     override suspend fun add(user: UserModel): UserModel {
         mutex.withLock {
