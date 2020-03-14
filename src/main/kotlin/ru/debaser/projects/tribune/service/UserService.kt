019e4262e91a18d15a7263898ba3cb4a0ceec39f
debaser <debaser86@yandex.ru>
2020-03-14 08:50:06 +0300

    Create UserService

---
+++ src/main/kotlin/ru/debaser/projects/tribune/service/UserService.kt
@@ -0,0 +1,23 @@
+package ru.debaser.projects.tribune.service
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import ru.debaser.projects.tribune.UserRepository
+import ru.debaser.projects.tribune.dto.AuthenticationRequestDto
+import ru.debaser.projects.tribune.dto.AuthenticationResponseDto
+import ru.debaser.projects.tribune.model.UserModel
+
+class UserService (
+    private val repo: UserRepository,
+    private val tokenService: JWTTokenService
+) {
+    private val mutex = Mutex()
+
+    suspend fun register(input: AuthenticationRequestDto): AuthenticationResponseDto {
+        mutex.withLock {
+            val user = repo.add(UserModel(username = input.username, password = input.password))
+            val token = tokenService.generate(user.id)
+            return AuthenticationResponseDto(user.id, token)
+        }
+    }
+}
