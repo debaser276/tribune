628cae861b1f3f4b3726200fee873a0ce738af3f
debaser <debaser86@yandex.ru>
2020-03-14 08:39:01 +0300

    Del getById from Repository

--- src/main/kotlin/ru/debaser/projects/tribune/UserRepository.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/UserRepository.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.sync.withLock
 import ru.debaser.projects.tribune.model.UserModel
 
 interface UserRepository {
-    suspend fun getByid(id: Long): UserModel?
     suspend fun add(user: UserModel): UserModel
     suspend fun save(user: UserModel): UserModel
 }
@@ -14,8 +13,6 @@ class UserRepositoryInMemoryWithMutex: UserRepository {
     private var nextId = 1L
     private val users = mutableListOf<UserModel>()
     private val mutex = Mutex()
-
-    override suspend fun getByid(id: Long): UserModel? = users.find { it.id == id }
 
     override suspend fun add(user: UserModel): UserModel {
         mutex.withLock {
