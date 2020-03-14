a662b5c2f1a7c84bc28ac9575c587138c7f840ad
debaser <debaser86@yandex.ru>
2020-03-14 10:00:21 +0300

    Add getregistration route

--- src/main/kotlin/ru/debaser/projects/tribune/route/RoutingV1.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/route/RoutingV1.kt
@@ -1,9 +1,29 @@
 package ru.debaser.projects.tribune.route
 
+import io.ktor.application.call
+import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.post
+import io.ktor.routing.route
+import ru.debaser.projects.tribune.Exceptions.LoginAlreadyExistsException
+import ru.debaser.projects.tribune.dto.AuthenticationRequestDto
+import ru.debaser.projects.tribune.service.UserService
 
-class RoutingV1() {
+class RoutingV1(
+    val userService: UserService
+) {
     fun setup(configuration: Routing) {
-
+        with(configuration) {
+            route("/api/v1") {
+                post("/registration") {
+                    val input = call.receive<AuthenticationRequestDto>()
+                    when(userService.getByUsername(input.username)) {
+                        null -> call.respond(userService.register(input))
+                        else -> throw LoginAlreadyExistsException()
+                    }
+                }
+            }
+        }
     }
 }
