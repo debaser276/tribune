19524a70acf3745975afec367683e8775a5ecb61
debaser <debaser86@yandex.ru>
2020-03-14 08:49:51 +0300

    Create JWTTokenService

---
+++ src/main/kotlin/ru/debaser/projects/tribune/service/JWTTokenService.kt
@@ -0,0 +1,17 @@
+package ru.debaser.projects.tribune.service
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import java.util.*
+
+class JWTTokenService (private val secret: String) {
+    private val algo = Algorithm.HMAC256(secret)
+
+    val verifier: JWTVerifier = JWT.require(algo).build()
+
+    fun generate(id: Long): String = JWT.create()
+        .withClaim("id", id)
+        .withExpiresAt(Calendar.getInstance().apply { add(Calendar.HOUR, 1) }.time)
+        .sign(algo)
+}
