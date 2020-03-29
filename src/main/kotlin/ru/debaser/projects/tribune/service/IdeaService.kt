908f8e992e77ac52326157284c9405b591cede09
debaser <debaser86@yandex.ru>
2020-03-29 21:30:46 +0300

    Add mutex to IdeaService

--- src/main/kotlin/ru/debaser/projects/tribune/service/IdeaService.kt
+++ src/main/kotlin/ru/debaser/projects/tribune/service/IdeaService.kt
@@ -1,5 +1,7 @@
 package ru.debaser.projects.tribune.service
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import ru.debaser.projects.tribune.dto.IdeaResponseDto
 import ru.debaser.projects.tribune.exception.DatabaseException
 import ru.debaser.projects.tribune.exception.IdeaNotFoundException
@@ -12,6 +14,8 @@ class IdeaService (
     private val ideaRepo: IdeaRepository,
     private val voteRepo: VoteRepository
 ) {
+    private val mutex = Mutex()
+
     suspend fun postIdea(idea: IdeaModel): Long =
         ideaRepo.postIdea(idea) ?: throw DatabaseException()
 
@@ -19,8 +23,10 @@ class IdeaService (
         ideaRepo.getById(id) ?: throw IdeaNotFoundException()
 
     suspend fun like(ideaId: Long, userId: Long): IdeaResponseDto {
-        ideaRepo.like(ideaId, userId)
-        voteRepo.addVote(userId, ideaId, true)
-        return IdeaResponseDto.fromModel(getById(ideaId))
+        mutex.withLock {
+            ideaRepo.like(ideaId, userId)
+            voteRepo.addVote(userId, ideaId, true)
+            return IdeaResponseDto.fromModel(getById(ideaId))
+        }
     }
 }
