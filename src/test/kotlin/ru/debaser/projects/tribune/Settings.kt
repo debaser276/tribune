58ce88e3f184fc1f0d3bf874923618274fb5e427
debaser <debaser86@yandex.ru>
2020-04-03 18:09:44 +0300

    Add TestContainers

--- src/test/kotlin/ru/debaser/projects/tribune/Settings.kt
+++ src/test/kotlin/ru/debaser/projects/tribune/Settings.kt
@@ -4,17 +4,23 @@ import io.ktor.application.Application
 import io.ktor.config.MapApplicationConfig
 import io.ktor.http.ContentType
 import io.ktor.http.withCharset
+import org.testcontainers.containers.PostgreSQLContainer
 import java.nio.file.Files
 
 class Settings {
     companion object {
+        class AppPostgreSQLContainer : PostgreSQLContainer<AppPostgreSQLContainer>("postgres:latest")
         val jsonContentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
         private val uploadPath = Files.createTempDirectory("test").toString()
+        private val postgresContainer = AppPostgreSQLContainer().apply {
+            start()
+        }
         val configure: Application.() -> Unit = {
             (environment.config as MapApplicationConfig).apply {
                 put("tribune.upload.dir", uploadPath)
                 put("tribune.jwt.secret", "2875f2518dd74feeb3260ebe1d24cb09")
-                put("tribune.db.jdbcUrl", "postgres://debaser:password@localhost:54321/test")
+//                put("tribune.db.jdbcUrl", "postgres://debaser:password@localhost:54321/test")
+                put("tribune.db.jdbcUrl", "postgres://${postgresContainer.username}:${postgresContainer.password}@${postgresContainer.containerIpAddress}:${postgresContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)}/${postgresContainer.databaseName}")
                 put("tribune.settings.reader-dislikes", "1")
                 put("tribune.settings.result-size", "20")
             }
